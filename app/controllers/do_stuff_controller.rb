4e922e800bd8a4b1250857d3242d2c71da0893d5
Robin Daugherty <robin@robindaugherty.net>
2015-12-20 02:00:22 -0500

    Add some untested code

---
+++ app/controllers/do_stuff_controller.rb
@@ -0,0 +1,8 @@
+class DoStuffController < ApplicationController
+  def thing1
+    a = 1
+    b = 2
+    c = 3
+    d = a + b + c
+  end
+end
---
+++ spec/controllers/do_stuff_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe DoStuffController, type: :controller do
+
+end
