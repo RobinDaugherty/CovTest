ffb397dc18abc58d6d163c4d3aab2b32800f7f51
Robin Daugherty <robin@robindaugherty.net>
2016-01-31 15:50:11 -0500

    Cover do_stuff#thing1

--- spec/controllers/do_stuff_controller_spec.rb
+++ spec/controllers/do_stuff_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe DoStuffController, type: :controller do
-
+  describe 'GET thing1' do
+    let(:the_request) { get :thing1 }
+    it 'responds' do
+      expect(the_request).to have_http_status(:ok)
+    end
+  end
 end
